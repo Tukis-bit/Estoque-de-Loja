348898256bd8a261522f15d516d6432a1b95f814
Tukis-bit <arthur01raphael@gmail.com>
2025-06-18 15:52:46 -0300

    Começo do projeto

---
+++ Estoque/app.js
@@ -0,0 +1,12 @@
+import prompt from 'prompt-sync'
+import * as funcoes from './funcoes.js';
+const ler = prompt();
+
+
+funcoes.apresentarPrograma();
+
+for(let i =1; i != 0; i++){
+funcoes.apresentarMenu();
+let resposta = Number(ler()); 
+
+break}
---
+++ Estoque/funcoes.js
@@ -0,0 +1,7 @@
+export function apresentarPrograma(){
+    console.log("-----------------------------------------------\n-   Seja bem vindo à Loja de Jogos do Tukis   -\n-----------------------------------------------");
+}
+
+export function apresentarMenu(){
+    console.log('---------------------------------\n-   1. Cadastrar Novo Produto   -\n-   2. Listar Produtos          -\n-   3. Adicionar ao Estoque     -\n-   4. Retirar Do estoque       -\n-   5. Apagar Produto           -\n-   0. Sair Do Programa         -\n---------------------------------');
+} 
