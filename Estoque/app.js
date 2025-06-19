ce5de9694c7b0f07a0e3f0a472a045c257f9ce86
Tukis-bit <arthur01raphael@gmail.com>
2025-06-19 14:19:40 -0300

    meio mennu feito

--- Estoque/app.js
+++ Estoque/app.js
@@ -2,6 +2,8 @@ import prompt from 'prompt-sync'
 import * as funcoes from './funcoes.js';
 const ler = prompt();
 
+let jogos = [];
+
 
 funcoes.apresentarPrograma();
 
@@ -9,4 +11,21 @@ for(let i =1; i != 0; i++){
 funcoes.apresentarMenu();
 let resposta = Number(ler()); 
 
-break}
+//Adicionar um jogo no estoque
+if(resposta == 1){
+jogos.push(funcoes.adicionarJogo());
+}
+
+else if(resposta == 2){
+    // Faz uma lista dos jogos disponíveis
+    funcoes.listarJogos(jogos);
+
+    console.log('\nEscolha um jogo de acordo com o seu número');
+    resposta = Number(ler());
+
+    //Mostra o jogo escolhido 
+    funcoes.mostrarJogo(resposta,jogos);
+
+    break}
+}
+
--- Estoque/funcoes.js
+++ Estoque/funcoes.js
@@ -1,3 +1,6 @@
+import prompt from"prompt-sync"
+const ler = prompt();
+
 export function apresentarPrograma(){
     console.log("-----------------------------------------------\n-   Seja bem vindo à Loja de Jogos do Tukis   -\n-----------------------------------------------");
 }
@@ -5,3 +8,50 @@ export function apresentarPrograma(){
 export function apresentarMenu(){
     console.log('---------------------------------\n-   1. Cadastrar Novo Produto   -\n-   2. Listar Produtos          -\n-   3. Adicionar ao Estoque     -\n-   4. Retirar Do estoque       -\n-   5. Apagar Produto           -\n-   0. Sair Do Programa         -\n---------------------------------');
 } 
+
+export function adicionarJogo(){
+     console.log('Me informe o nome do jogo ');
+    let nome = ler();
+
+    console.log('\nMe informe a impresa criadora do jogo');
+    let emp = ler();
+
+    console.log('\nMe informe o tamanho do jogo em Gigas');
+    let tamanho = Number(ler());
+
+    console.log('\nMe informe o preço do jogo ');
+    let preco = Number(ler());
+
+    console.log('\nMe informe a quantidade de jogos para compra ');
+    let quant = Number(ler());
+
+    let jogo = { 
+        nomeJogo: nome,
+        tamanhoJogo: tamanho,
+        empresa: emp,
+        precoJogo: preco,
+        quantidade: quant}
+
+return jogo;
+}
+
+export function listarJogos(jogos){
+    console.log("Os jogos No menu são:");
+    let i = 1; 
+
+    for(let item of jogos){
+      
+      console.log(`${i}. ${item.nomeJogo}`)
+      i++
+    }
+
+}
+
+export function mostrarJogo(resp,jogos){
+     
+    console.log(`Jogo: ${jogos[resp -1].nomeJogo}`);
+    console.log(`Tamanho: ${jogos[resp -1].tamanhoJogo}`);
+    console.log(`empresa: ${jogos[resp -1].empresa}`);
+    console.log(`Preço: R$ ${jogos[resp -1].precoJogo}`);
+    console.log(`Quantidade para compra: ${jogos[resp -1].quantidade}`);
+}
